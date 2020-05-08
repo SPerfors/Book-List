31839c86fefce9856c30ca0e6c9088b7dde8b6c4
SPerfors <stevecanbuild@gmail.com>
2020-05-08 12:00:09 -0500

    created two helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,16 @@ class ApplicationController < Sinatra::Base
        erb :home    
     end
 
+    helpers do
+        
+        def logged_in?
+            !!session[:user_id]
+        end
+
+        def current_user
+            User.find(session[:user_id])
+        end
+
+    end
+
 end
