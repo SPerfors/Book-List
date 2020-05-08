99da574fcbbaadd6b3ca8be976058eee6a1b3873
SPerfors <stevecanbuild@gmail.com>
2020-05-08 12:08:55 -0500

    created post signup

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -8,4 +8,17 @@ class UsersController < ApplicationController
         end
     end
 
+    post '/signup' do 
+        user = User.new(username: params[:username], password: params[:password], email: params[:email])
+
+        if user[:username].empty? || user[:password_digest] == nil || user[:email].empty?
+            redirect '/signup'
+        else
+            user.save
+            session[:user_id] = user.id 
+            redirect '/repository'
+        end
+    end
+
+
 end
