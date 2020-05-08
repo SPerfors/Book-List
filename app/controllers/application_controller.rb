1d7033c4433d2504e4a86fb32595772cac6b98be
SPerfors <stevecanbuild@gmail.com>
2020-05-08 11:12:29 -0500

    created application_controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base 
+
+    configure do 
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "secret"
+        use Rack::Flash 
+    end
+
+
+end
