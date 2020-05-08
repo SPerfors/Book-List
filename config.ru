a0b88db1aaa9e0c3697384d3fa8390ad4c5ab8d4
SPerfors <stevecanbuild@gmail.com>
2020-05-08 11:31:56 -0500

    fixed errors to run home page

--- config.ru
+++ config.ru
@@ -1,1 +1,3 @@
 require './config/environment'
+
+run ApplicationController
--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'sinatra/json'
 
 ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3"
-    :database => "db/book_development.sqlite"
+    #:database => "db/book_development.sqlite"
 )
 
-require_all 'app'
+require_all "app"
