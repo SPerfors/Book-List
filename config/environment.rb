99e8d612c696cc16cd767eb8d664fdf628db61be
SPerfors <stevecanbuild@gmail.com>
2020-05-08 11:05:30 -0500

    create environment

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+require "bundler"
+Bundler.require
+require 'sinatra/json'
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3"
+    :database => "db/book_development.sqlite"
+)
+
+require_all 'app'
