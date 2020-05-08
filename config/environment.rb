f642233e0ac2eddd4c050ac2003eab6e3affd558
SPerfors <stevecanbuild@gmail.com>
2020-05-08 12:40:09 -0500

    cleaned up environment code

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,7 @@ Bundler.require
 require 'sinatra/json'
 
 ActiveRecord::Base.establish_connection(
-    #:adapter => "sqlite3"
-    #:database => "db/book_development.sqlite"
+    
 )
 
 require_all "app"
