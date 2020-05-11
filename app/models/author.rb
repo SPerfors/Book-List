2cee13003ed58e330a6b8b2e45eb0c49b293ab88
SPerfors <stevecanbuild@gmail.com>
2020-05-11 13:46:35 -0500

    updated database routing

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base 
+    belongs_to :users
     has_many :books 
     has_many :genres, through: :books 
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base 
     has_secure_password
-    
+    has_many :authors
+    has_many :books
+    has_many :genres, through: :books 
+
 end
