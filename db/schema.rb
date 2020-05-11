d458969a179087f5011bfac7ba60564b35d5589b
SPerfors <stevecanbuild@gmail.com>
2020-05-11 13:40:12 -0500

    changed authors column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_182902) do
+ActiveRecord::Schema.define(version: 2020_05_11_183917) do
 
   create_table "authors", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "book_genres", force: :cascade do |t|
