b04f26c4c4bca2aa1b182ec11daaf2509e676248
SPerfors <stevecanbuild@gmail.com>
2020-05-12 14:02:58 -0500

    clean up sytling

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,7 @@ class AuthorsController < ApplicationController
 
     get "/authors" do 
         @authors = Author.all 
+        @user = current_user
         erb :"/authors/index"
     end
     
--- app/controllers/repository_controller.rb
+++ app/controllers/repository_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
     get "/repository" do 
+        @user = current_user
         erb :"/repository/repository"
     end
 
